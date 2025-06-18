ace5dbe725693a86014260ebd6ac472f121f2d62
david-rai <davidkhaling123@gmail.com>
2025-06-18 13:33:02 +0545

    cerebras explored for chat complemation

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import OpenAI from 'openai'
+
+dotenv.config()
+const app=express()
+
+//Middlewares for json and form parsing
+app.use(cors())
+app.use(express.json());
+
+app.get('/',async (req, res) => {
+
+  const client = new OpenAI({
+    baseURL: "https://api.cerebras.ai/v1",
+    apiKey:process.env.KEY,
+  });
+  
+  const chatCompletion = await client.chat.completions.create({
+    model: "qwen-3-32b",
+      messages: [
+          {
+              role: "user",
+              content: "What is the gitub",
+          },
+      ],
+  });
+  
+  console.log(chatCompletion.choices[0].message);
+  
+  res.json(chatCompletion.choices[0].message.content);
+});
+
+const PORT=process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port PORT`);
+});
